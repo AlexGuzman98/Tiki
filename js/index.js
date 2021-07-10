fd2084a602ae65e065f578fc9d27f8eebfc7edb8
Fabian Barquero <fabianbarquero9@gmail.com>
2021-07-10 00:34:13 -0600

    Agrego API de artistas

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
+/* eslint-disable no-alert */
 //
 // Tiki Music - Fabian Barquero & Alex Guzman
 //
 
-const p1 = document.getElementById('p1');
-const p2 = document.getElementById('p2');
-const p1Value = p1.value.trim();
-const p2Value = p2.value.trim();
+// API artist
 
-if (p1Value !== p2Value) {
-  alert('Las passwords deben de coincidir');
-}
+const URL = 'https://kt2ul4cwza.execute-api.us-east-2.amazonaws.com/public/artists';
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((data) => {
+    const infoContent = document.querySelector('.home__content ul');
+    for (let i = 0; i < data.length; i += 1) {
+      console.log(data[i]);
+
+      const listContent = document.createElement('li');
+      infoContent.appendChild(listContent);
+
+      const dataArtist = `
+        <img src="${data[i].image}" alt="${data[i].id} cover">
+        <h2>${data[i].name}</h2>
+      `;
+
+      listContent.innerHTML = dataArtist;
+    }
+  });
