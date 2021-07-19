d0b6365d215d0c1c514e47bf52e5a88b02e9f572
AlexGuzman98 <alexd.guzman98@gmail.com>
2021-07-19 14:56:51 -0600

    agregoModal

--- js/artistContent.js
+++ js/artistContent.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/no-cycle
 import { artistApi } from './apiArtist.js';
-
+import { modalPlaylist } from './modal.js'
+modalPlaylist();
 artistApi();
 class ArtistPrincipal {
   constructor(data) {
@@ -11,12 +14,10 @@ class ArtistPrincipal {
     for (let i = 0; i < this.data.length; i += 1) {
       const listContent = document.createElement('li');
       infoContent.appendChild(listContent);
-
-      const dataArtist = `
-      <a href=""><img src="${this.data[i].image}" alt="${this.data[i].id} cover">
-      <h2>${this.data[i].name}</h2></a>
+      listContent.innerHTML = `
+      <a href=""><img src="${this.data[i].image}" alt="${this.data[i].id} cover"></a>
+      <a href=""><h2>${this.data[i].name}</h2></a>
   `;
-      listContent.innerHTML = dataArtist;
     }
   }
 }
