588ea18502d28799b43f2b7ee60243ce65bf0449
Fabian Barquero <fabianbarquero9@gmail.com>
2021-08-06 14:00:51 -0600

    Agrego cambios en favorites

--- js/playlist.js
+++ js/playlist.js
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-parens */
 const url = 'https://tiki-music.herokuapp.com/favorite/6100bbcdec3538f60593b5b2';
 // ${id}
 
@@ -13,7 +14,15 @@ function playlist() {
         fetch(`https://kt2ul4cwza.execute-api.us-east-2.amazonaws.com/public/song/${element}`)
           .then((response) => response.json())
           .then((data) => {
-            console.log(data);
+            // console.log(data);
+            const favoriteTab = document.querySelector('#tab-5 ul');
+            for (let i = 0; i < this.data.length; i += 1) {
+              const favoriteContent = document.createElement('li');
+              favoriteTab.appendChild(favoriteContent);
+              favoriteContent.innerHTML = `
+              
+          `;
+            }
           });
       });
     });
