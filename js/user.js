55054a850a61be90a3d7398d9e4a2df20124a789
AlexGuzman98 <alexd.guzman98@gmail.com>
2021-08-09 15:59:11 -0600

    fix

--- js/user.js
+++ js/user.js
@@ -2,7 +2,7 @@ function user() {
   fetch('https://tiki-music.herokuapp.com/user/610dce509df4c800049a8e3b')
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById('user_name').innerHTML = `${data.data.name}`;
+      document.getElementById('user_name').innerHTML = `Welcome ${data.data.name}`;
     });
 }
 
